ca9f6cf11dddc5acc2eca8290c5de4257aaa3162
apple <apple@apples-MacBook-Air.local>
2023-10-09 10:00:57 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Snake Game
+
+
+https://user-images.githubusercontent.com/89828255/183551280-76351f9c-c77e-4f3c-8e50-0036f9aa8424.mp4
+
---
+++ lib/HomeBinding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+import 'HomeController.dart';
+
+class HomeBinding extends Bindings{
+  @override
+  void dependencies() {
+    Get.lazyPut(() => HomeController());
+  }
+
+}
